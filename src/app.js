22039fe9abf201a588da34ba99ef689ea8a70511
Vansh Rana <vanshrana96669@gmail.com>
2026-04-12 19:12:55 +0530

    adding register route and login route

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 const express = require("express");
-
+const authRouter= require("./routes/auth.routes")
 const app = express();
+app.use(express.json())
 
-app.use(express.json());
+/* require all routes  use here*/
+app.use("/api/auth", authRouter)
+
+
 
 module.exports = app;
---
+++ src/routes/auth.routes.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const authRouter = express.Router()
+const authController= require("../controllers/auth.controller")
+
+authRouter.post("/register", authController.registerUserController)
+authRouter.post("/login", authController.loginUserController)
+
+
+module.exports= authRouter
